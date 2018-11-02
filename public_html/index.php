9535fcc2f6f7d29c112f093d33b49355224e4445
Abdul Shaddad <abdul.shaddad@comm-press.de>
2018-11-02 10:28:17 +0100

    nba-1: modified the docker-compose file

--- public_html/index.php
+++ public_html/index.php
@@ -1,13 +1,22 @@
-<h1>Hello Cloudreach!</h1>
 <h4>Attempting MySQL connection from php...</h4>
 <?php
-$host = 'mysql';
-$user = 'root';
-$pass = 'rootpassword';
-$conn = new mysqli($host, $user, $pass);
+$host = 'DB';
+$user = 'admin';
+$pass = '123';
+$database_name= 'npa';
+$port = '3306';
+$conn = mysqli_connect($host, $user, $pass, $database_name, $port );
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 echo "Connected to MySQL successfully!";
+$sqlTest = "SELECT * FROM user";
+$result_sql = mysqli_query($conn, $sqlTest);
+while ($row = mysqli_fetch_array($result_sql, MYSQLI_ASSOC)) {
+    $rows[] = $row;
+    print_r($rows);
+
+}
+
 ?>
