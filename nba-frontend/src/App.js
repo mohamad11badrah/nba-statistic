e1c268d795c427ecbb79730b91413ba1438d2607
Mohamad . Badrah <mohamad@lionizers.com>
2018-11-13 18:00:34 +0100

    Nba localapi (#3)
    * add localapi to call DB
    
    * call localapi from client

--- nba-frontend/src/App.js
+++ nba-frontend/src/App.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ApiURL from './apiClient';
+import $ from 'jquery';
 
 class App extends Component {
-  render() {
+  state = {
+    userID: null,
+    userName: null,
+  }
+  componentDidMount() {
+    return $.ajax ({
+      url: ApiURL('/user'),
+      type: 'GET',
+      dataType: 'json',
+      contentType: 'application/json',
+      success: data => this.setState({ userID: data[0].id , userName: data[0].name}),
+      error: error => console.log(error),
+    });
+  }
+
+  render() {    
+    console.log(this.state);
+    
     return (
       <div className="App">
         <header className="App-header">
@@ -17,7 +36,8 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            dont Learn React
+            <p>user name is {this.state.userName}</p>
+            <p>user ID is {this.state.userID}</p>
           </a>
         </header>
       </div>
