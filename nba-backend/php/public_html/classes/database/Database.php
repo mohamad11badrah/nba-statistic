b239399225db9362d994bd1dfc93c63690e954a8
Mohamad . Badrah <mohamad@lionizers.com>
2018-11-16 13:38:57 +0100

    Remove confilct (#4)
    * change port number
    
    * nba-1: modified the docker-compose file
    
    * add a nba-backend folder
    
    * changed the dir of nba-backend
    
    * insert data to the database
    
    * modified insert into database
    
    * create the database

---
+++ nba-backend/php/public_html/classes/database/Database.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Class Database
+ * Attribute $connection as privet.
+ * Methods:
+ * __construct: make a connection with the database.
+ * getConnection: return the database connection.
+ */
+
+class Database
+{
+    private $_connection;
+    /**
+    * Database constructor.
+    * Pars the ini-File.
+    * Open a connection with the database.
+    */
+    public function __construct()
+    {
+        $db = parse_ini_file('settings.ini');
+        $connect = mysqli_connect($db['host'], $db['user'], $db['pass'], $db['name'], $db['port']);
+
+        $this->_connection = $connect;
+        if (!$this->_connection) {
+            print "Es gibt keine Verbindung mit der Datenbank ";
+        }else {
+            print "Es gibt Verbindung";
+        }
+    }
+    /**
+    * Return the database connection
+    *
+    * @return mysqli
+    */
+    public function getConnection()
+    {
+        return $this->_connection;
+    }
+}
+
