fbbece9b1771f01dd91634b35d624ef36b70e008
mohamad11badrah <mohamad@lionizers.com>
2018-11-20 15:21:46 +0100

    ádd components

--- nba-frontend/src/components/Header.js
+++ nba-frontend/src/components/Header.js
@@ -6,20 +6,23 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import '../style/Header.css';
 import ball from '../images/ball.png';
+import nba from '../images/nba.png';
 
 
 class Header extends Component {
 
  render() {
-
      return(
          <div className='main-head'>
-            <AppBar position="static" >
-               <Toolbar>
-                   <img src={ball} className="head-logo" alt="logo" />
-                 <Button color="inherit">Login</Button>
-               </Toolbar>
-      </AppBar>
+            <img src={ball} className="head-logo" alt="logo" />
+            <img src={nba} className='head-logo-img' alt='nba'/>
+            <div className='head-button'>
+                 <Button color="inherit">Teams</Button>
+                 <Button color="inherit">players</Button>
+                 <Button color="inherit">games</Button>
+                 <Button color="inherit">reports</Button>
+                 <Button color="inherit">chart</Button>
+            </div>
          </div>
      );
  }
