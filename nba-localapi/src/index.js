926458a3d851d8bf9d0a1933c67059ee6f5664ca
mohamad11badrah <mohamad@lionizers.com>
2018-11-20 18:25:01 +0100

    fetch data from tables in localapi

--- nba-localapi/src/index.js
+++ nba-localapi/src/index.js
@@ -15,7 +15,23 @@ connection.connect(function(err) {
   console.log('You are now connected...')
   connection.query('SELECT * FROM player', function(err, results) {
     if (err) throw err
-    app.get('/user' , (req, res) => res.send(results));
-    app.listen(port, () => console.log(`Listening on port ${port}`));
+    app.get('/players' , (req, res) => res.send(results));
   })
+  connection.query('SELECT * FROM games', function(err, results) {
+    if (err) throw err
+    app.get('/games' , (req, res) => res.send(results));
+  })
+  connection.query('SELECT * FROM report', function(err, results) {
+    if (err) throw err
+    app.get('/reports' , (req, res) => res.send(results));
+  })
+  connection.query('SELECT * FROM depthCharts', function(err, results) {
+    if (err) throw err
+    app.get('/depth' , (req, res) => res.send(results));
+  })
+  connection.query('SELECT * FROM team', function(err, results) {
+    if (err) throw err
+    app.get('/teams' , (req, res) => res.send(results));
+  })
+  app.listen(port, () => console.log(`Listening on port ${port}`));
 })
